bcf1ed78321190afbd3c8185adbb7dea291be144
delia1205 <75974731+delia1205@users.noreply.github.com>
2025-01-31 13:39:51 +0200

    added open api swagger documentation on auth-api

--- backend/auth-api/controllers/auth.controller.js
+++ backend/auth-api/controllers/auth.controller.js
@@ -2,7 +2,6 @@ import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
-import Cookies from "js-cookie";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -57,65 +56,8 @@ export const signin = async (req, res, next) => {
     );
 
     const { password: pass, ...rest } = validUser._doc;
-    res
-      .status(200)
-      // .cookie("token", token, {
-      //   httpOnly: true,
-      //   secure: process.env.NODE_ENV === "production",
-      //   sameSite: "None",
-      //   path: "/",
-      //   maxAge: 60 * 60 * 24 * 7,
-      // })
-      .json({ token, ...rest });
+    res.status(200).json({ token, ...rest });
   } catch (error) {
     next(error);
   }
 };
-
-// export const google = async (req, res, next) => {
-//   const { name, email, googlePhotoURL } = req.body;
-
-//   try {
-//     const user = await User.findOne({ email });
-//     if (user) {
-//       const token = jwt.sign(
-//         { id: user._id, isAdmin: user.isAdmin },
-//         process.env.JWT_KEY
-//       );
-//       const { password: pass, ...rest } = user._doc;
-//       res
-//         .status(200)
-//         .cookie("token", token, {
-//           httpOnly: true,
-//         })
-//         .json(rest);
-//     } else {
-//       const generatedPassword =
-//         Math.random().toString(36).slice(-8) +
-//         Math.random().toString(36).slice(-8);
-//       const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
-//       const newUser = new User({
-//         username:
-//           name.toLowerCase().split(" ").join("") +
-//           Math.random().toString(9).slice(-4),
-//         email,
-//         password: hashedPassword,
-//         photoURL: googlePhotoURL,
-//       });
-//       await newUser.save();
-//       const token = jwt.sign(
-//         { id: newUser._id, isAdmin: newUser.isAdmin },
-//         process.env.JWT_KEY
-//       );
-//       const { password: pass, ...rest } = newUser._doc;
-//       res
-//         .status(200)
-//         .cookie("token", token, {
-//           httpOnly: true,
-//         })
-//         .json(rest);
-//     }
-//   } catch (error) {
-//     next(error);
-//   }
-// };
--- backend/auth-api/index.js
+++ backend/auth-api/index.js
@@ -5,6 +5,8 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 dotenv.config();
 
@@ -29,12 +31,29 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3002, () => {
-  console.log("Server is running on port 3002.");
-});
+// Swagger setup
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Auth API",
+      version: "1.0.0",
+      description: "API for user authentication and management",
+    },
+  },
+  apis: ["auth-api/routes/*.js"],
+};
+
+const specs = swaggerJSDoc(options);
+console.log(JSON.stringify(specs, null, 2));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.listen(3002, () => {
+  console.log("Server is running on port 3002.");
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
