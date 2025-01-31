2c157b40808bfe233aca53854b89e70f3d699ca0
delia1205 <75974731+delia1205@users.noreply.github.com>
2025-01-31 02:04:23 +0200

    fixed dropdown responsiveness from navbar

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -4,61 +4,82 @@ import { UserContext } from "../hooks/context";
 import Title from "./Title";
 
 export default function Layout() {
-  let { userData } = useContext(UserContext);
+  const { userData } = useContext(UserContext);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
 
   return (
     <>
-      {userData.email === "" ? (
-        <nav className="see-through">
-          <div className="title">
-            <Title />
-          </div>
-          <button
-            className="dropdown"
-            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-          >
-            Dropdown
-          </button>
-          <ul className="list">
+      <nav className={"see-through"}>
+        <div className="title">
+          <Title />
+        </div>
+
+        <ul className="list">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          {userData.email === "" ? (
+            <>
+              <li>
+                <Link to="/sign-in">Sign in</Link>
+              </li>
+              <li>
+                <Link to="/tutorial">Tutorial</Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/query">Query</Link>
+              </li>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link to="/tutorial">Tutorial</Link>
+              </li>
+            </>
+          )}
+        </ul>
+
+        <button className="icon" onClick={toggleDropdown}>
+          &#9776;
+        </button>
+
+        {isDropdownOpen && (
+          <ul className="dropdown">
             <li>
               <Link to="/">Home</Link>
             </li>
-            <li>
-              <Link to="/sign-in">Sign in</Link>
-            </li>
-            <li>
-              <Link to="/tutorial">Tutorial</Link>
-            </li>
+            {userData.email === "" ? (
+              <>
+                <li>
+                  <Link to="/sign-in">Sign in</Link>
+                </li>
+                <li>
+                  <Link to="/tutorial">Tutorial</Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/query">Query</Link>
+                </li>
+                <li>
+                  <Link to="/profile">Profile</Link>
+                </li>
+                <li>
+                  <Link to="/tutorial">Tutorial</Link>
+                </li>
+              </>
+            )}
           </ul>
-        </nav>
-      ) : (
-        <nav className="see-through">
-          <div className="title">
-            <Title />
-          </div>
-          <button
-            className="dropdown"
-            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-          >
-            Dropdown
-          </button>
-          <ul className="list">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/query">Query</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="/tutorial">Tutorial</Link>
-            </li>
-          </ul>
-        </nav>
-      )}
+        )}
+      </nav>
 
       <Outlet />
     </>
